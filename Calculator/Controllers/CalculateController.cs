66ed49a06f2782e8c790f83c4b88c29fef4ef939
jagdeepsingh84 <A00222002@mycambrian.ca>
2020-12-19 15:53:48 +0530

    webAPI

---
+++ Calculator/Controllers/CalculateController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleCalc;
+
+namespace Calculator.Controllers
+{
+    public class CalculateController : System.Web.Mvc.Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(int num1,int num2,string operation)
+        {
+            SimpleCalc.SimpleCalc simpleCalc = new SimpleCalc.SimpleCalc();
+            ViewData["result"] = simpleCalc.OperatorSwitch(num1,num2,operation);
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SimpleCalc/SimpleCalc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SimpleCalc
+{
+    public class SimpleCalc
+    {
+        public int add(int leftNumber, int rightNumber)
+        {
+            return leftNumber + rightNumber;
+        }
+        public int sub(int leftNumber, int rightNumber)
+        {
+            return leftNumber - rightNumber;
+        }
+        public int multi(int leftNumber, int rightNumber)
+        {
+            return leftNumber * rightNumber;
+        }
+        public int divide(int leftNumber, int rightNumber)
+        {
+            return leftNumber / rightNumber;
+        }
+        public string OperatorSwitch(int leftNumber, int rightNumber, string operation)
+        {
+            int result = 0;
+            bool errorState = false;
+            switch (operation)
+            {
+                case "+":
+                    result = leftNumber + rightNumber;
+                    break;
+                case "-":
+                    result = leftNumber - rightNumber;
+                    break;
+                case "*":
+                    result = leftNumber * rightNumber;
+                    break;
+                case "/":
+                    result = rightNumber != 0 ? leftNumber / rightNumber : 0;
+                    errorState = rightNumber != 0 ? false : true;
+                    break;
+                default:
+                    errorState = true;
+                    break;
+            }
+
+            string resultStr = "";
+            resultStr += result;
+            if (errorState)
+            {
+                resultStr = "error!. divided by zero";
+            }
+            return resultStr;
+        }
+    }
+}
